e2fca840ceda0468678e703571a96ef4be8a32b2
Daniel Rose <daniel@lastcallmedia.com>
2021-05-12 10:06:03 -0400

    Update recon.sh

--- bash/README.md
+++ bash/README.md
@@ -5,3 +5,9 @@
 Example: `./ping_sweep.sh 192.168.2` (Only need the first 3 octects of the target IP)
 
 This will ping the IP range from 1 - 254 to see which hosts are up.
+
+### recon.sh
+
+Example: `./recon.sh 192.168.2.1`
+
+This runs nmap, gobuster, and whatweb on a host and saves results into results file.
--- bash/recon.sh
+++ bash/recon.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 
-# Script runs nmap and whatweb on a host and saves results into results file
+# Script runs nmap, gobuster, and whatweb on a host and saves results into results file
 
+
+#!/bin/bash
 
 if [ -z "$1" ]
 then
-		echo "Usage: ./recon.sh <IP>"
-		exit 1
+        echo "Usage: ./recon.sh <IP>"
+        exit 1
 fi
 
-printf "\n----NMAP----\n\n" > results
+printf "\n----- NMAP -----\n\n" > results
 
-echo "Running Nmap"
+echo "Running Nmap..."
 nmap $1 | tail -n +5 | head -n -3 >> results
 
 while read line
 do
-	if [[ $line == *open* ]] && [[ $line == *http* ]]
-	then
-	echo "Running WhatWeb..."
-	whatweb $1 -v > temp
-	fi
+        if [[ $line == *open* ]] && [[ $line == *http* ]]
+        then
+                echo "Running Gobuster..."
+                gobuster dir -u $1 -w /usr/share/wordlists/dirb/common.txt -qz > temp1
+
+        echo "Running WhatWeb..."
+        whatweb $1 -v > temp2
+        fi
 done < results
 
-if [ -e temp]
+if [ -e temp1 ]
 then
-		printf "\n----Web----\n\n" >> results
-		cat temp >> results
-		rm temp
+        printf "\n----- DIRS -----\n\n" >> results
+        cat temp1 >> results
+        rm temp1
+fi
+
+if [ -e temp2 ]
+then
+    printf "\n----- WEB -----\n\n" >> results
+        cat temp2 >> results
+        rm temp2
 fi
 
 cat results
